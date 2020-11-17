35022dece57db35aaf13a8cbfaa263a26bc60636
Erick-EEV <EEVERICKEEV@gmail.com>
2020-11-16 20:40:32 -0500

    Basic math js

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+let num1 = 31
+let num2 = 2
+
+let multiply = num1 * num2
+
+let random = Math.random()
+
+let num3 = 16
+let num4 = 4
+
+let mod = 16 % 4
+
+let max = Math.max(1, 3, 6, 9, 12, 16, 20)
